cd9f5885ba5755bf3cde8a35b8ee668e66fb5dea
Vesali <e0006008@u.nus.edu>
2017-04-02 13:19:08 +0800

    Add AppIcon

--- src/main/java/seedu/tasklist/ui/MainWindow.java
+++ src/main/java/seedu/tasklist/ui/MainWindow.java
@@ -23,7 +23,7 @@ import seedu.tasklist.model.UserPrefs;
  */
 public class MainWindow extends UiPart<Region> {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/flexiTaskAppIcon.png";
     private static final String FXML = "MainWindow.fxml";
     private static final int MIN_HEIGHT = 600;
     private static final int MIN_WIDTH = 450;
