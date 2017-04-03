818c929eec91d0bf10acf70d35a991b69006ef87
sherinatoh <sherinatohh@gmail.com>
2017-04-04 03:55:57 +0800

    add Field IDs to UpcomingTaskCardHandle

--- src/test/java/guitests/UpcomingTaskCardHandle.java
+++ src/test/java/guitests/UpcomingTaskCardHandle.java
@@ -1,5 +1,9 @@
 package guitests;
 
-public class UpcomingTaskCardHandle extends GuiHandle {
+import guitests.guihandles.GuiHandle;
 
+public class UpcomingTaskCardHandle extends GuiHandle {
+    private static final String NAME_FIELD_ID = "#name";
+    private static final String DATE_ONE_FIELD_ID = "#firstDate";
+    private static final String DATE_TWO_FIELD_ID = "#secondDate";
 }
