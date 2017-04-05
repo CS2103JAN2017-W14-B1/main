41fc96b57eab06620364cbbcb0640171bfb071e3
Vesali <e0006008@u.nus.edu>
2017-04-05 14:29:23 +0800

    Update java docs of Task.java

--- src/main/java/seedu/tasklist/model/task/Task.java
+++ src/main/java/seedu/tasklist/model/task/Task.java
@@ -8,22 +8,49 @@ import seedu.tasklist.model.tag.UniqueTagList;
  */
 public abstract class Task implements ReadOnlyTask {
 
+    /**
+     * Sets name of task
+     */
     public abstract void setName(Name name);
 
+    /**
+     * Returns name of task
+     */
     public abstract Name getName();
 
+    /**
+     * Sets comment for a task
+     */
     public abstract void setComment(Comment comment);
 
+    /**
+     * Returns comment for a task
+     */
     public abstract Comment getComment();
 
+    /**
+     * Returns the tag list of task
+     */
     public abstract UniqueTagList getTags();
 
+    /**
+     * Sets priority of a task
+     */
     public abstract void setPriority(Priority priority);
 
+    /**
+     * Returns Priority of task
+     */
     public abstract Priority getPriority();
 
+    /**
+     * Sets Status of task
+     */
     public abstract void setStatus(Status status);
 
+    /**
+     * Returns Status of task
+     */
     public abstract Status getStatus();
 
     /**
