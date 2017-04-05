c9b927be6aa36c8c77fa633af51599f5cf1d5ca9
Vesali <e0006008@u.nus.edu>
2017-04-05 18:17:12 +0800

    Add more test cases for SortCommand.java

--- src/test/java/guitests/SortCommandTest.java
+++ src/test/java/guitests/SortCommandTest.java
@@ -2,10 +2,17 @@ package guitests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.text.ParseException;
+
 import org.junit.Test;
 
+import guitests.guihandles.TaskCardHandle;
 import seedu.tasklist.commons.core.Messages;
+import seedu.tasklist.commons.exceptions.IllegalValueException;
 import seedu.tasklist.logic.commands.SortCommand;
+import seedu.tasklist.testutil.DeadlineTaskBuilder;
+import seedu.tasklist.testutil.EventTaskBuilder;
+import seedu.tasklist.testutil.FloatingTaskBuilder;
 import seedu.tasklist.testutil.TestTask;
 //@@author A0141993X
 public class SortCommandTest extends TaskListGuiTest {
@@ -52,10 +59,71 @@ public class SortCommandTest extends TaskListGuiTest {
         commandBox.runCommand(commandPriority);
         assertResultMessage(SortCommand.MESSAGE_SUCCESS);
         assertSortSuccess(expectedListPriority);
+
+        //sort by date after adding floating task
+        try {
+            TestTask floatingTaskToAdd = new FloatingTaskBuilder().withName("Shop clothes")
+                    .withTags("shopping").withComment("save money")
+                    .withPriority("low").withStatus(false).build();
+            TestTask[] expectedListAddFloating = {td.tutorial, td.project, td.groceries,
+                                                  td.homework, td.drink, td.java, floatingTaskToAdd};
+            commandBox.runCommand("add Shop clothes t/shopping c/save money p/low");
+            commandBox.runCommand("sort d");
+            assertResultMessage(SortCommand.MESSAGE_SUCCESS);
+            assertSortSuccessAdd(floatingTaskToAdd, expectedListAddFloating);
+
+        } catch (IllegalValueException e) {
+            e.printStackTrace();
+        }
+
+        //sort by date after adding deadline task
+        try {
+            TestTask deadlineTaskToAdd = new DeadlineTaskBuilder().withName("Project Report Submission")
+                    .withTags("work").withComment("discuss with teammates")
+                    .withDeadline("15 April 6pm").withPriority("high").withStatus(false).build();
+            TestTask[] expectedListAddDeadline = {td.tutorial, td.project, deadlineTaskToAdd, td.groceries,
+                                                  td.homework, td.drink, td.java};
+            commandBox.runCommand("add Project Report Submission d/15 April 6pm "
+                                   + "t/work c/discuss with teammates p/high");
+            commandBox.runCommand("sort d");
+            assertResultMessage(SortCommand.MESSAGE_SUCCESS);
+            assertSortSuccessAdd(deadlineTaskToAdd, expectedListAddDeadline);
+        } catch (IllegalValueException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //sort by date after adding event task
+        try {
+            TestTask eventTaskToAdd = new EventTaskBuilder().withName("Project Meeting")
+                    .withTags("work").withComment("discuss with teammates")
+                    .withStartDate("12 April 4pm").withEndDate("12 April 6pm")
+                    .withPriority("high").withStatus(false).build();
+            TestTask[] expectedListAddEvent = {td.tutorial, td.project, eventTaskToAdd,
+                                               td.groceries, td.homework, td.drink, td.java};
+            commandBox.runCommand("add Project Meeting d/12 April 4pm to 6pm "
+                    + "t/work c/discuss with teammates p/high");
+            commandBox.runCommand("sort d");
+            assertResultMessage(SortCommand.MESSAGE_SUCCESS);
+            assertSortSuccessAdd(eventTaskToAdd, expectedListAddEvent);
+        } catch (IllegalValueException ive) {
+            ive.printStackTrace();
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
     }
 
     private void assertSortSuccess(TestTask... expectedList) {
         //confirm list contains all tasks in the correct sorted order
+        assertTrue(taskListPanel.isListMatching(expectedList));
+    }
+    
+    private void assertSortSuccessAdd(TestTask taskToAdd, TestTask... expectedList) {
+      //confirm the new card contains the right data
+        TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getName().fullName);
+        assertMatching(taskToAdd, addedCard);
+
         assertTrue(taskListPanel.isListMatching(expectedList));
     }
 
