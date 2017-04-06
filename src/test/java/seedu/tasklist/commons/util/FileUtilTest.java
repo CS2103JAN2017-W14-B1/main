d5261d069a6c8c5790a5e238afad543964d19a3d
Vesali <e0006008@u.nus.edu>
2017-04-06 18:29:39 +0800

    Add test cases to improve code coverage

--- src/test/java/seedu/tasklist/commons/util/FileUtilTest.java
+++ src/test/java/seedu/tasklist/commons/util/FileUtilTest.java
@@ -1,8 +1,11 @@
 package seedu.tasklist.commons.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,6 +29,35 @@ public class FileUtilTest {
         // no forwards slash -> assertion failure
         thrown.expect(AssertionError.class);
         FileUtil.getPath("folder");
+    }
+//@@author A0141993X
+    @Test
+    public void isFileExists_fileExists_returnsTrue() throws IOException {
+        String filePath = "filePathExists.xml";
+        File file = new File(filePath);
+        FileUtil.createFile(file);
+        assertTrue(FileUtil.isFileExists(file));
+    }
+
+    @Test
+    public void isFileExists_fileDoesNotExist_returnsFalse() {
+        String filePath = "fileDoesNotExist.xml";
+        File file = new File (filePath);
+        assertFalse(FileUtil.isFileExists(file));
+    }
+
+    @Test
+    public void createDirs_validDirectory_successful() throws IOException {
+        String dir = "Users/test/saveData";
+        File dirFile = new File(dir);
+        FileUtil.createDirs(dirFile);
+    }
+
+    @Test  (expected = IOException.class)
+    public void createDirs_invalidDirectory_throwsException() throws IOException {
+        String dir = "test< \0? save";
+        File dirFile = new File(dir);
+        FileUtil.createDirs(dirFile);
     }
 
 }
