af7bb932ca06d5f1f5e8bf2eae392379eac97dfe
sherinatoh <sherinatohh@gmail.com>
2017-04-09 16:26:05 +0800

    add PriorityTest

---
+++ src/test/java/seedu/tasklist/model/task/PriorityTest.java
@@ -0,0 +1,23 @@
+package seedu.tasklist.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PriorityTest {
+
+    @Test
+    public void isValidPriority() {
+        //valid Priority
+        assertTrue(Priority.isValidPriority("high"));
+        assertTrue(Priority.isValidPriority("medium"));
+        assertTrue(Priority.isValidPriority("low"));
+
+        //invalid Priority
+        assertFalse(Priority.isValidPriority("important"));
+        assertFalse(Priority.isValidPriority(""));
+        assertFalse(Priority.isValidPriority("not important"));
+
+    }
+}
