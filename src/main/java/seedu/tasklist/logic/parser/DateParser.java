e993c37e93a934523f27ba8ec3acd2fbb33f656e
sherinatoh <sherinatohh@gmail.com>
2017-04-09 18:24:59 +0800

    change date regex to flip ddmm to mmdd

--- src/main/java/seedu/tasklist/logic/parser/DateParser.java
+++ src/main/java/seedu/tasklist/logic/parser/DateParser.java
@@ -2,6 +2,8 @@ package seedu.tasklist.logic.parser;
 
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.ocpsoft.prettytime.nlp.PrettyTimeParser;
 
@@ -13,11 +15,12 @@ public class DateParser {
     public static final String NO_DATES_MESSAGE = "No dates were detected, please enter a valid date after /d.";
     public static final String EXCESS_DATES = "Please enter only one date for Deadlines and two dates for Events.";
 
+    private static Pattern ddmmRegex = Pattern.compile("\\b(0?[12][0-9]|3[01])[/-](0?[1-9]|1[012])\\b");
     /**
      * Uses PrettyTime NLP library to parse the dates into a list of Date objects
      */
     public static List<Date> parse (String date) throws IllegalValueException {
-        List<Date> dates = new PrettyTimeParser().parse(date);
+        List<Date> dates = new PrettyTimeParser().parse(changeDateFormat(date));
         if (dates.isEmpty()) {
             throw new IllegalValueException(NO_DATES_MESSAGE);
         }
@@ -25,5 +28,23 @@ public class DateParser {
             throw new IllegalValueException(EXCESS_DATES);
         }
         return dates;
+    }
+
+    /**
+     * PrettyTime parser reads dates in MMDD format
+     * Changes date to that format to be read by PrettyTimeParser
+     * @param date is the String for date captured by the parser
+     * @return returns the string with DDMM swapped to be MMDD
+     */
+    private static String changeDateFormat(String date) {
+        Matcher ddmm = ddmmRegex.matcher(date);
+        while (ddmm.find()) {
+            String dateFound = ddmm.group();
+            String day = ddmm.group(1);
+            String month = ddmm.group(2);
+            String changedDate = month + "/" + day;
+            date = date.replaceAll(dateFound, changedDate);
+        }
+        return date;
     }
 }
